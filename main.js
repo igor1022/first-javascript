70de7c79663e094c592768506aeb5bbe391340b6
unknown <imitroshichev@gmail.com>
2022-05-28 11:27:07 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+/*const el = document.querySelector('input[name="people"]');
+el.innerHTML = 'fgh';
+
+console.log(el);
+*/
+const el2 = document.querySelector('input[name="people"]');
+
+el2.addEventListener('change', (ev) => {
+    const text = ev.target.value;
+    console.log(text);
+    const el1 = document.querySelector('.try');
+    el1.innerHTML = text;
+    
+}
+)
